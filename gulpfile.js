8858c442a9633f1518fca693fd013a6b080ca420
Juan C. Olivares <cristobal@cxsoftware.com>
2014-12-09 07:56:31 -0300

    Adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require ('gulp');
+var traceur = require ('gulp-traceur');
+
+gulp.task('default', function () {
+	return gulp.src ('src/*.js')
+		.pipe (traceur (
+		{
+			'modules': 'inline',
+			'asyncFunctions': true
+		}))
+		.pipe (gulp.dest ('dist'));
+});
