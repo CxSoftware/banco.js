449a54bafe1ed359a65bef426306fd2b191740d8
Juan C. Olivares <cristobal@cxsoftware.com>
2014-12-09 14:09:46 -0300

    Adding more methods Adding some logging for testing

--- src/banks/BciPersonas.js
+++ src/banks/BciPersonas.js
@@ -7,13 +7,24 @@ var config = {
 	balanceUrl: 'https://www.bci.cl/cl/bci/aplicaciones/menu/vistas/inicio/miBanco.jsf'
 };
 
+// Local variables
+var ph = null;
+var page = null;
+
+// Local functions
+var check = () =>
+{
+	if (ph == null || page == null)
+			throw Error ("Please log in first");
+}
+
 // Methods
 module.exports =
 {
 	login: async (username, password) =>
 	{
-		var ph = await phantom.createAsync ();
-		var page = await ph.createPageAsync ();
+		ph = await phantom.createAsync ();
+		page = await ph.createPageAsync ();
 		await page.openAsync (config.loginUrl);
 		await page.evaluateAsync (data =>
 		{
@@ -24,12 +35,38 @@ module.exports =
 			$('#frm').submit ();
 		}, { username: username, password: password });
 		await page.waitLoad ();
-		await page.waitLoad ();
+	},
+	getAccounts: async () =>
+	{
 		await page.openAsync (config.balanceUrl);
 		await page.waitLoad ();
+		var accounts = await page.evaluateAsync (() =>
+			jQuery.map (
+				jQuery('option'),
+				x => jQuery(x).val()));
+		return accounts.map (x =>
+		{
+			return { id: x, name: x };
+		});
+	},
+	getBalance: async () =>
+	{
+		await page.openAsync (config.balanceUrl);
+		await page.waitLoad ();
+		await page.evaluateAsync (id =>
+			jQuery ('option')
+				.filter (x => jQuery (x).val () == id)
+				.attr ('selected', true));
 		var balance = await page.evaluateAsync (() =>
 			jQuery ('.montos strong').first().text());
+		return parseInt (balance
+			.substring (2)
+			.replace ('.', ''));
+	},
+	dispose: () =>
+	{
 		ph.exit ();
-		return balance;
+		ph = null;
+		page = null;
 	}
 };
--- src/test.js
+++ src/test.js
@@ -10,6 +10,18 @@ var readline = require ('./async/readline');
 	// Lgin
 	var username = await readline.ask ('Username: ');
 	var password = await readline.ask ('Password: ');
-	var balance = await bci.login (username, password);
-	console.log (balance);
+	console.log ('Loging in...');
+	await bci.login (username, password);
+
+	console.log ('Getting accounts...');
+	var accounts = await bci.getAccounts ();
+	console.log (accounts);
+	if (accounts.length == 0)
+		throw Error ('No accounts found!');
+
+	console.log ('Getting balance');
+	var balance = await bci.getBalance (accounts [0].id);
+	console.log ("Balance for", accounts[0].name, balance);
+
+	bci.dispose ();
 })();
